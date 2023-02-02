4e9a61800bf61b9a64d6dc5fad9f857e2203e92e
TCHIPOTOU Kossivi Désiré <desire.tchipotou@gmail.com>
2023-02-02 17:24:29 +0000

    Movies views, New launch URL, First service : IActorsService

---
+++ eTickets/Data/Services/Interfaces/IActorsService.cs
@@ -0,0 +1,18 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services.Interfaces
+{
+    public interface IActorsService
+    {
+        // Get all Actors.
+        IEnumerable<Actor> GetAll();
+        // Detail or Show : Get acor by it's id.
+        Actor GetById(int id);
+        // Add a new actor.
+        void Add(Actor actor);
+        // Update an existing actor.
+        Actor Update(int id, Actor newActor);
+        // Delete an actor.
+        void Delete(int id);
+    }
+}
